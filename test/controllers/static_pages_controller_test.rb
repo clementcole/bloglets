35103c7972b4ebc264cf87e20a663bbd8b8e5517
Clement Cole <clementacole75@gmail.com>
2015-06-14 20:11:12 +0000

    Started Annoucments and Login Pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get Announcements" do
     get :Announcements
     assert_response :success
+    assert_select "title", "Announcements | Bloglets"
   end
 
   test "should get Login" do
     get :Login
     assert_response :success
+    assert_select "title", "Login | Bloglets"
   end
 
 end
