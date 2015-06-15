7a3e2818bd707baba5ceac87b93506915f08b9ff
Clement Cole <clementacole75@gmail.com>
2015-06-15 01:00:54 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
 
   get 'users/new'
 
-  root 'static_pages#announcements'
-  get 'login' => 'static_pages#login'
-  get 'weekly' => 'static_pages#weekly'
-  get 'signup' => 'users#new'
+  get 'users/new'
+
+  root  'static_pages#announcements'
+  get   'weekly' => 'static_pages#weekly'
+  get   'signup' => 'users#new'
+  get   'login' => 'sessions#new'
+  post  'login' => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
   resources :users
 
 end
