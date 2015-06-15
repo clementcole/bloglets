4446c511470da07a98b0fbb5b55f7fa6f06dce01
Clement Cole <clementacole75@gmail.com>
2015-06-15 04:48:50 +0000

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150614225035) do
+ActiveRecord::Schema.define(version: 20150615043002) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150614225035) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
